35f32365b803c53daa6c84358776f995f12973cd
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-11 21:35:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/11
+# @Author  : 2984922017@qq.com
+# @File    : config.py
+# @Software: PyCharm
+
+## 配置说明
+## CK和UA信息需自行抓包,我的 -> 任务中心 -> 领券中心
+## 抓包地址:https://store.oppo.com/cn/oapi/users/web/checkPeople/isNewPeople
+# {
+#     'user':'',    # 备注
+#     'CK':'',      # Cookie
+#     'UA':''       # User-Agent
+# },
+
+accounts = [
+    {
+        'user':'',
+        'CK':'',
+        'UA':''
+    },
+    {
+        'user':'',
+        'CK':'',
+        'UA':''
+    }
+]
