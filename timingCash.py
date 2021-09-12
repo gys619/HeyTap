324ddff5d1739826833866458abe4109d7047b0b
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-12 17:12:06 +0800

    Update timingCash.py

--- timingCash.py
+++ timingCash.py
@@ -140,7 +140,7 @@ if __name__ == '__main__':
                     timingCash.start()
                     break
                 except requests.exceptions.ConnectionError:
-                    print(f"{timingCash.dic['user']}\t请求失败，随机延迟后再次访问")
+                    logger.info(f"{timingCash.dic['user']}\t请求失败，随机延迟后再次访问")
                     time.sleep(random.randint(2,5))
                     continue
             else:
