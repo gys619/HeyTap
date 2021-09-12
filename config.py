78bb298b4b2ec2899d37622cd931f14d3d5beacd
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-12 12:58:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 # @Software: PyCharm
 
 ## 配置说明
-## CK和UA信息需自行抓包,我的 -> 任务中心 -> 领券中心
+## CK和UA信息需自行抓包,欢太商城 -> 我的 -> 任务中心 -> 领券中心
 ## 抓包地址:https://store.oppo.com/cn/oapi/users/web/checkPeople/isNewPeople
 # {
 #     'user':'',    # 备注
