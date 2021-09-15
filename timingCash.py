525533cc24e25a9794cb61e54cf86354783663f2
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-15 21:10:02 +0800

    Update timingCash.py

--- timingCash.py
+++ timingCash.py
@@ -6,12 +6,13 @@
 # @Software: PyCharm
 import os
 import sys
+import json
 import time
 import random
 import logging
 
 # 配置文件
-from config import accounts, timeCash_LOG_PATH
+from config import admin, accounts, timeCash_LOG_PATH
 
 # 日志模块
 logger = logging.getLogger(__name__)
@@ -118,6 +119,29 @@ class TimingCash:
                 self.getCash(dic=each)
                 break
 
+    # pushPlus配信
+    @staticmethod
+    def sendForPush():
+        if [each for each in admin['mask'] if each == os.path.basename(__file__)[:-3]] == []:
+            with open(file=LOG_FILE,mode='r',encoding='utf-8') as f:
+                content = f.read()
+            url = 'http://www.pushplus.plus/send'
+            data = {
+                "token": admin['pushGroup']['pushToken'],
+                "title":"易班打卡通知",
+                "content":content,
+                "template":"txt"
+            }
+            if admin['pushGroup']['pushToken'] != "":
+                if admin['pushGroup']['pushTopic'] != "":
+                    data['topic'] = admin['pushGroup']['pushTopic']
+                response = requests.post(url=url,data=json.dumps(data)).json()
+                if response['code'] == 200:
+                    logger.info(f"Push Plus发信成功！\n")
+                else:
+                    logger.info(f"Push Plus发信失败！\t失败原因:{response['msg']}")
+            else:
+                logger.info(f"未配置pushPlus Token,取消配信！")
 
     # 执行欢太商城实例对象
     def start(self):
@@ -148,3 +172,4 @@ if __name__ == '__main__':
             else:
                 logger.info(f"账号: {timingCash.dic['user']}\n状态: 取消登录\n原因: 多次登录失败")
                 break
+    TimingCash.sendForPush()
