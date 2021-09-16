7a48bfcd96c41952475585d447f97f8ae2397e27
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-16 16:53:03 +0800

    Update dailyCash.py

--- dailyCash.py
+++ dailyCash.py
@@ -38,7 +38,7 @@ stream.setFormatter(logFormat)
 logger.addHandler(stream)
 
 # 日志录入时间
-logger.info(f"时间:{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())}")
+logger.info(f"任务:{'天天赚积分'}\n时间:{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())}")
 
 class DailyCash:
     def __init__(self,dic):
@@ -287,7 +287,7 @@ class DailyCash:
             url = 'http://www.pushplus.plus/send'
             data = {
                 "token": admin['pushGroup']['pushToken'],
-                "title":"易班打卡通知",
+                "title":"欢太任务通知",
                 "content":content,
                 "template":"txt"
             }
